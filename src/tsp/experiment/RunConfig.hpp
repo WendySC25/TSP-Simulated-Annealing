36199fb75c27b31ddfba1f6b9d97765af2f0ec0b
WendySC <wendy.c2502@gmail.com>
2026-09-10 22:03:10 -0600

    feat: Run Config struct to track experimenting parameters

---
+++ src/tsp/experiment/RunConfig.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+struct RunConfig {
+    std::string label;
+    int         seed;
+
+    // Initial Temperature parameters
+    double targetP;
+    double epsilonP;
+    double initialT;
+    int    tempSample;
+    
+    // Simulated Annealing
+    int    batch;
+    int    maxAttempts;
+    double coolingFactor;
+    double epsilon;
+};
+
+
