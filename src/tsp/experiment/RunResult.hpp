ad656c2257e0cf136871f17a1ec0f5f1827a37b0
WendySC <wendy.c2502@gmail.com>
2026-09-10 22:04:48 -0600

    feat: Run Result strucht to save and report relevant values as the final cost and path solution

---
+++ src/tsp/experiment/RunResult.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "RunConfig.hpp"
+
+struct RunResult {
+    RunConfig        config;
+    std::vector<int> finalPath;   
+    std::string      timestamp;   
+
+    double    initialCost        = 0.0;   
+    double    initialTemperature = 0.0;
+    double    finalCost          = 0.0;
+    long long elapsedMs          = 0;
+};
