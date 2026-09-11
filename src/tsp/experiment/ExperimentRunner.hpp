d38c78a6ad3024355d6fc8c4683a796579261617
WendySC <wendy.c2502@gmail.com>
2026-09-10 23:11:38 -0600

    feat: Definition of Experiment Runner class to manage experiments multithreads

---
+++ src/tsp/experiment/ExperimentRunner.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+#include "RunConfig.hpp"
+#include "RunResult.hpp"
+#include "tsp/core/SolutionTSP.hpp"
+#include "tsp/heuristic/InitialTemperature.hpp"
+#include "tsp/heuristic/SimulatedAnnealing.hpp"
+
+class ExperimentRunner {
+    private:
+        const std::vector<std::vector<double> > &matrix;
+        double normalizer;
+        int    numThreads;
+        
+        RunResult runOne(const RunConfig &cfg);
+        void worker(const std::vector<RunConfig> &configs, std::vector<RunResult> &results, size_t begin, size_t end);
+
+    public:
+        ExperimentRunner(const std::vector<std::vector<double> > &matrix, double normalizer, int numThreads = 0);
+        std::vector<RunResult> run(const std::vector<RunConfig> &configs);
+};
